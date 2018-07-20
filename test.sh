b36d9719ada047d5b05beea22a6cf95d4838cf73
hieunvce <hieunv.ce@gmail.com>
2018-07-20 12:19:30 +0700

    Test done on PC

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-FILES=../images/tomatos/*
+FILES=./images/tomatos/*
 for f in $FILES
 do
     echo "Processing $f file..."
-    ./TomatoClassification $f >> pc.txt
+    ./build/TomatoClassification $f >> pc.txt
 done
 echo "Finished"
